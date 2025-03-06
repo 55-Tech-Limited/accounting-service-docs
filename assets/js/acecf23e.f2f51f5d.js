6c5b453fb0b2c74bb10acd7b2c977faa26d86951
dev-davexoyinbo <dev-davexoyinbo@users.noreply.github.com>
2025-03-06 19:46:59 +0000

    deploy: 5b00c58c992c448753508e5128057640c935dc93

---
+++ assets/js/acecf23e.f2f51f5d.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkdocs=self.webpackChunkdocs||[]).push([[1903],{1912:s=>{s.exports=JSON.parse('{"blogBasePath":"/accounting-service-docs/blog","blogTitle":"Blog","authorsListPath":"/accounting-service-docs/blog/authors"}')}}]);
