d57b7ff04128e0a8d4c34a694f364c13e405d34a
David Oyinbo <dev.davexoyinbo@gmail.com>
2025-03-11 18:49:19 +0100

    remove features

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,7 +3,6 @@ import clsx from "clsx";
 import Link from "@docusaurus/Link";
 import useDocusaurusContext from "@docusaurus/useDocusaurusContext";
 import Layout from "@theme/Layout";
-import HomepageFeatures from "@site/src/components/HomepageFeatures";
 import Heading from "@theme/Heading";
 
 import styles from "./index.module.css";
@@ -12,7 +11,10 @@ function HomepageHeader() {
   const { siteConfig } = useDocusaurusContext();
   return (
     <header className={clsx("hero hero--primary", styles.heroBanner)}>
-      <div className="container" style={{ paddingTop: "3em", paddingBottom: "3em" }}>
+      <div
+        className="container"
+        style={{ paddingTop: "3em", paddingBottom: "3em" }}
+      >
         <Heading as="h1" className="hero__title">
           {siteConfig.title}
         </Heading>
@@ -35,9 +37,7 @@ export default function Home(): ReactNode {
       description="Description will go into a meta tag in <head />"
     >
       <HomepageHeader />
-      <main>
-        <HomepageFeatures />
-      </main>
+      <main></main>
     </Layout>
   );
 }
